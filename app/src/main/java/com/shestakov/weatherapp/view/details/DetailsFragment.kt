fffbca8ff0487a25190421b4773b6554d4c50057
Evgeniy Shestakov <Gennadith.bear@gmail.com>
2021-06-28 22:19:04 +0500

    homework-5

--- app/src/main/java/com/shestakov/weatherapp/view/details/DetailsFragment.kt
+++ app/src/main/java/com/shestakov/weatherapp/view/details/DetailsFragment.kt
@@ -1,38 +1,73 @@
 package com.shestakov.weatherapp.view.details
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.shestakov.weatherapp.R
 import com.shestakov.weatherapp.databinding.FragmentDetailsBinding
 import com.shestakov.weatherapp.model.Weather
+import com.shestakov.weatherapp.model.WeatherDTO
+
 
 class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var weatherBundle: Weather
+    private val onLoadListener: WeatherLoader.WeatherLoaderListener =
+        object : WeatherLoader.WeatherLoaderListener {
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+
+            override fun onFailed(throwable: Throwable) {
+                Log.e("onLoadListener.OnFailed",
+                    getString(R.string.server_connection_error), throwable)
+                throwable.printStackTrace()
+                Snackbar.make(
+                    binding.mainView,
+                    getString(R.string.server_connection_error),
+                    Snackbar.LENGTH_INDEFINITE).show()
+            }
+        }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val weather = arguments?.getParcelable<Weather>(BUNDLE_EXTRA)
-        if (weather != null) {
-            val city = weather.city
-            binding.cityName.text = city.cityName
-            binding.cityCoordinates.text = String.format(
-                    getString(R.string.city_coordinates),
-                    city.lat.toString(), city.lon.toString()
+        weatherBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: Weather()
+        with(binding) {
+            mainView.visibility = View.GONE
+            loadingLayout.visibility = View.VISIBLE
+        }
+        val loader = WeatherLoader(onLoadListener, weatherBundle.city.lat, weatherBundle.city.lon)
+        loader.loadWeather()
+    }
+
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            mainView.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+            val city = weatherBundle.city
+            cityName.text = city.cityName
+            cityCoordinates.text = String.format(getString(R.string.city_coordinates),
+                city.lat.toString(), city.lon.toString()
             )
-            binding.temperatureValue.text = weather.temperature.toString()
-            binding.feelsLikeValue.text = weather.feelsLike.toString()
+            weatherCondition.text = weatherDTO.fact?.condition
+            temperatureValue.text = weatherDTO.fact?.temp.toString()
+            feelsLikeValue.text = weatherDTO.fact?.feels_like.toString()
         }
     }
 
@@ -44,5 +79,10 @@ class DetailsFragment : Fragment() {
             fragment.arguments = bundle
             return fragment
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
